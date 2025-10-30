4c482b08abb511edded1bace1743ee828408b4c2
Biruk B <103274295+gitbiruk2010@users.noreply.github.com>
2025-10-29 18:39:54 -0700

    fix error file to test pre-commit hook

--- nsc-events-nextjs/components/RemoveTest.tsx
+++ nsc-events-nextjs/components/RemoveTest.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 const TestLintError = () => {
-  const badObject = {property:'value'};
+  const badObject = { property: 'value' };
   
-  if (true) console.log('Error');
+  if (true) 
+    {console.log('Error');}
     
   return <div>Test Component with Linting Errors</div>;
 };
 
 export default TestLintError;
+
+// added line to test.
